f23976cbf36cc5c54a71216478d7850dca25fe7a
Garret Blankenship <garret@leafcutter.com.au>
2018-12-20 12:46:35 +1100

    first init

---
+++ src/ColourSelector.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+class ColourSelector extends Component {
+    onInputChange = (event) => {
+        const { onColourSelectorChange } = this.props;
+        onColourSelectorChange(event.target.value);
+    }
+    
+    render() {
+        const { hex } = this.props;
+        
+        return (
+            <input 
+                type="color" 
+                value={hex} 
+                onChange={this.onInputChange} 
+            />
+        );
+    }
+}
+
+ColourSelector.defaultProps = {
+   hex: "#ffff00"
+};
+
+export default ColourSelector;
